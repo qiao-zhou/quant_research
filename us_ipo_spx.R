e2cd5c5ec6a41dbe16fb48b2e451d7b1b975696f
z080328 <zhouqiao_ntu@yahoo.com>
2016-04-20 07:35:17 +0800

    Data and code for the IPO related research

---
+++ us_ipo_spx.R
@@ -0,0 +1,6 @@
+data_ipo = read.csv("C:/dev/quant_research/data/RITTER-US_IPO_STATS.csv", header = TRUE)
+data_spx = read.csv("C:/dev/quant_research/data/SPX.csv", header = TRUE)
+
+head(data_ipo)
+head(data_spx)
+
